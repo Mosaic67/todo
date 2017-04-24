0886fdd8a420ace5a80d11b386d557dc6c7bfa81
沙滨 <47492653@qq.com>
2017-04-24 19:08:36 +0800

    overwrite confirm function

--- js/base.js
+++ js/base.js
@@ -1,6 +1,8 @@
 ;(function() {
     'use strict';
     var $form_add_task = $('.add-task'),
+        $body = $('body'),
+        $window = $(window),
         $task_delete_trigger,
         $task_detail_trigger,
         $task_detail = $('.task-detail'),
@@ -10,10 +12,15 @@
         $update_form,
         $task_detail_content,
         $task_detail_content_input,
-        $checkbox_complete
+        $checkbox_complete,
+        $msg = $('.msg'),
+        $msg_content = $msg.find('.msg-content'),
+        $msg_confirm = $msg.find('.confirmed'),
+        $alerter = $('.alerter')
     ;
 
     init();
+
 
     $form_add_task.on('submit', on_add_task_form_submit);
     $task_detail_mask.on('click', hide_task_detail);
@@ -27,6 +34,121 @@
         if (add_task(new_task)) {
             $input.val(null);
         }
+    }
+
+    function confirm(arg) {
+        if (!arg) {
+            console.error('confirm title is required');
+        }
+        var conf = {},
+            $box,
+            $mask,
+            $title,
+            $content,
+            $confirm,
+            confirmed,
+            $cancel,
+            timer,
+            dfd
+        ;
+        dfd=$.Deferred();
+        if (typeof arg == 'string') {
+            conf.title = arg;
+        } else {
+            conf = $.extend(conf, arg);
+        }
+        $mask = $('<div></div>')
+            .css({
+                position: 'fixed',
+                background: 'rgba(0,0,0,.5)',
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0
+
+            });
+        $box = $('<div>' +
+            '<div class="confirm-title">'+ conf.title +'</div>' +
+            '<div class="confirm-content ">' +
+            '<div>' +
+            '<button style="margin-right: 5px" class="primary confirm">确定</button>' +
+            '<button class="cancel">取消</button></div>' +
+            '</div>' +
+            '</div>')
+            .css({
+                position: 'fixed',
+                color:'#444',
+                width: 300,
+                height: 'auto',
+                padding:'15px 0',
+                background: '#fff',
+                'border-radius': 5,
+                'box-shadow': '0 1px 2px rgba(0,0,0,.5)'
+            });
+        $title = $box.find('.confirm-title').css({
+            padding:'5px 10px',
+            'font-weight':900,
+            'font-size': 20,
+            'text-align':'center'
+        });
+        $content = $box.find(('.confirm-content')).css({
+            padding:'5px 10px',
+            'text-align':'center'
+        });
+
+        $confirm = $content.find('button.confirm');
+        $cancel = $content.find('button.cancel');
+
+        timer = setInterval(function() {
+            if (confirmed !== undefined) {
+                dfd.resolve(confirmed);
+                clearInterval(timer);
+                dismiss_confirm();
+            }
+        }, 50);
+
+        function dismiss_confirm() {
+            $mask.remove();
+            $box.remove();
+        }
+        $confirm.on('click',function() {
+            confirmed = true;
+        });
+        $cancel.on('click',on_cancel);
+        $mask.on('click',on_cancel);
+        function on_cancel() {
+            confirmed = false;
+        }
+        function adjust_box_position() {
+            var window_width = $window.width(),
+                window_height = $window.height(),
+                box_width = $box.width(),
+                box_height = $box.height(),
+                move_x,
+                move_y;
+            move_x = (window_width - box_width) / 2;
+            move_y = ((window_height - box_height) / 2) - 20;
+
+            $box.css({
+                left: move_x,
+                top: move_y
+            })
+        }
+
+        $window.on('resize', function() {
+            adjust_box_position();
+        });
+
+        $mask.appendTo($body);
+        $box.appendTo($body);
+        $window.resize();
+        return dfd.promise();
+    }
+
+    function listen_msg_event() {
+        $msg_confirm.on('click', function() {
+            hide_msg();
+        })
     }
 
     function listen_task_detail() {
@@ -89,13 +211,15 @@
             '</div>' +
             '</div>' +
             '<div class="remind input-item">' +
-            '<input name="remind_date" type="date" value="' + (item.remind_date || '') + '">' +
+            '<label style="margin-bottom: 10px">提醒时间</label>' +
+            '<input class="datetime" name="remind_date" type="text" value="' + (item.remind_date || '') + '">' +
             '</div>' +
             '<button class="input-item" type="submit">更新</button>' +
             '</form>'
         ;
         $task_detail.html(null).html(tpl);
         $update_form = $task_detail.find('form');
+        $('.datetime').datetimepicker();
         $task_detail_content = $update_form.find('.content');
         $task_detail_content_input = $update_form.find('[name=content]');
 
@@ -119,7 +243,10 @@
             var $this = $(this);
             var $item = $this.parent().parent();
             var index = $item.data('index');
-            confirm('确认删除？') ? delete_task(index) : null;
+            confirm('确认删除？')
+                .then(function(result) {
+                    result ? delete_task(index) : null;
+                })
         });
     }
 
@@ -141,10 +268,44 @@
     }
 
     function init() {
+        listen_msg_event();
         task_list = store.get('task_list') || [];
         if (task_list.length) {
             render_task_list();
+            task_remind_check();
         }
+    }
+
+    function task_remind_check() {
+        var current_timestamp;
+        var itl = setInterval(function() {
+            for (var i = 0; i < task_list.length; i++) {
+                var item = get(i), task_timestamp;
+                if (!item || !item.remind_date || item.informed) continue;
+                current_timestamp = (new Date()).getTime();
+                task_timestamp = (new Date(item.remind_date)).getTime();
+                if (current_timestamp - task_timestamp >= 1) {
+                    update_task(i, {informed: true});
+                    show_msg(item.content);
+                }
+            }
+        }, 500);
+    }
+
+    function show_msg(msg) {
+        if (!msg) return;
+        $msg_content.html(msg);
+        $alerter.get(0).play();
+        $msg.show();
+    }
+
+    function hide_msg() {
+        $msg.hide();
+        $alerter.get(0).pause();
+    }
+
+    function get(index) {
+        return store.get('task_list')[index];
     }
 
     function render_task_list() {
